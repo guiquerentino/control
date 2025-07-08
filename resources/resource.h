9a419e80956e12260c4699e8d4ea9a8b2bf6f1ef
Guilherme Querentino <74635462+guiquerentino@users.noreply.github.com>
2025-07-08 16:21:51 -0300

    Update resource.h

--- resources/resource.h
+++ resources/resource.h
@@ -1,3 +1,3 @@
-#define IDD_PASSWORD_DIALOG      100
-#define IDC_EDIT_PASSWORD        101
-#define IDI_APP                  IDI_APPLICATION
+#define IDI_APP                IDI_APPLICATION
+#define IDD_PASSWORD_DIALOG    100
+#define IDC_EDIT_PASSWORD      101
